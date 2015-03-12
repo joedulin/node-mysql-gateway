cf1390cb827258aada8c121be2e11f430b3007a0
Joe Dulin <joe.dulin@vitelity.com>
2015-03-12 11:44:03 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,171 @@
+node-mysql-gateway
+=============
+### Purpose
+The purpose of this library is to create an Gateway/Table class API for MySQL development. It uses the flixge/node-mysql library to create a pool cluster with a MASTER and a SLAVE tagged MySQL connection pool. From there it automatically generates gateway classes for all tables available from the SLAVE pool.
+
+This is simply a quick and easy way to implement CRUD for a database
+
+**Notice:** Currently only supports one MASTER/SLAVE database combo. Extending this should not be a problem, but I haven't gotten there yet.
+
+**More Important Notice:** I've just finished this/uploaded it. It hasn't been thoroughly tested yet
+
+## Usage
+#### Installation
+*Not currently in npm registry*
+``` bash
+npm install joedulin/node-mysql-gateway
+```
+
+### Initialize
+``` javascript
+var msyqlGateway = require('mysql-gateway'),
+    gateways = mysqlGateway.gateways,
+    table = mysqlGateway.table;
+
+//For demonstration purposes assume callback =
+var callback = function (err, results) {
+	console.log(results);
+};
+```
+
+### Gateways
+*Used primarily for SELECT type statements*
+``` javascript
+/* gateways.table1 is automatically created
+given your database has a table named table1 */
+var table1 = gateways.table1;
+//gateway.get(search_obj, callback);
+table1.get({ name: 'bob' }, function (err, results) {
+    console.log(results);
+});
+/*
+[ {
+	table: 'table1',
+	columns: [ 'id', 'name', 'description' ],
+	primaryKey: 'id',
+    id: 1,
+	name: 'bob',
+	description: 'Nothing special. Just Bob.',
+	save: [Function],
+	getColumnValues: [Function]
+} ]
+*/
+
+```
+
+##### search_obj
+The search object can be made up of a few things. Passing in an empty object {} will result in a query with no WHERE clause or modifiers.
+
+``` javascript
+table1.get({}, callback);
+// SELECT `id`, `name`, `description` FROM `table1`;
+```
+
+_SELECT `id`, `name`, `description` FROM table1;_
+
+You can build the WHERE clause by passing in "column name": "column value" in the search object.
+
+``` javascript
+table1.get({ name: 'bob', description: 'blarg' }, callback);
+// SELECT `id`, `name`, `description` FROM `table1` WHERE `name` = 'bob' AND `description` = 'blarg';
+```
+
+Alternatively, you can have the value in a column: value be an object with an operator and value.
+
+**Notice:** The operator portion of this is not escaped. I intend to implement this somehow, someday, but for now use at your own risk.
+
+``` javascript
+table1.get({ name: { operator: '!=', value: 'bob' }, description: { operator: 'LIKE', value: '%blarg%' }});
+// SELECT `id`, `name`, `description` FROM `table1` WHERE `name` != 'bob' AND `description` LIKE '%blarg%';
+```
+
+The search object also has some extra functionality built in.
+
+``` javascript
+var search_obj = {
+	description: {
+		operator: 'LIKE',
+		value: '%blarg%'
+	},
+    columns: [ 'name', 'description' ],
+	limit: 5,
+	offset: 2,
+	orderby: 'name',
+	orderby_direction: 'DESC'
+};
+table1.get(search_obj, callback);
+//SELECT `name`, `description` FROM `table1` WHERE `description` LIKE '%blarg%' ORDER BY `name` DESC LIMIT 5 OFFSET 2;
+```
+
+For simplicity there is also the function gateway.getByPk(value, callback);
+
+Note that results in the callback will be a single table object rather than an array
+
+``` javascript
+table1.getByPk(1, callback);
+// SELECT `id`, `name`, `description` FROM `table1` WHERE id = 1;
+```
+
+### Table objects
+
+Gateway results are an array of table objects. These are simply JSON objects with the column: value of the table plus a couple of extra bits of info / functions.
+
+#### save
+
+``` javascript
+var table_row = table('table1');
+console.log(table_row);
+
+/*
+{
+	table: 'table1',
+	columns: [ 'id', 'name', 'description' ],
+	primaryKey: 'id',
+	id: null,
+	name: null,
+	description: null,
+	save: [Function],
+	getColumnValues: [Function]
+}
+*/
+
+table_row.name = 'bob';
+table_row.description = 'some description';
+table_row.save(callback); //Insert
+
+// OR ------------------------------------
+
+gateways.table1.getById(1, function (err, result) {
+	result.name = 'Bob McFrob';
+	result.save(callback); //Update
+});
+```
+
+The determining factor between an insert and an update is whether or not the table_row[table_row.primaryKey] is null.
+
+#### deleteRecord
+
+``` javascript
+gateways.table1.getById(1, function (err, result) {
+	result.deleteRecord(callback);
+});
+
+// Will delete the record based on result.primaryKey
+// DELETE FROM `table1` WHERE `id` = 1;
+```
+
+#### getColumnValues
+
+``` javascript
+gateways.table1.getById(1, function (err, result) {
+	console.log(result.getColumnValues());
+});
+
+/*
+{
+	id: 1
+	name: 'bob',
+	description: 'some description'
+}
+*/
+```
---
+++ index.js
@@ -0,0 +1,387 @@
+var pool = require('./lib/mysql-connection.js');
+var clone = require('clone');
+var deasync = require('deasync');
+var tables = {};
+
+function gateway (table, obj, callback) {
+	if (!table) {
+		return false;
+	}
+	obj = obj || {};
+	var self = {};
+
+	//table itself
+	self.table = '';
+
+	//columns / keys
+	self.columns = [];
+	self.primaryKey = 'id';
+
+	self.getConnection = function (db, callback) {
+		db = db || 'SLAVE';
+		callback = callback || function () { return false; };
+		pool.getConnection(db, function (err, conn) {
+			callback(err, conn);
+		});
+		return self;
+	};
+
+	self.query = function (db, q, values, callback, conn) {
+		db = db || 'SLAVE';
+		values = values || [];
+		callback = callback || function () { return false };
+
+		if (!conn) {
+			self.getConnection(db, function (err, connection) {
+				self.query(db, q, values, callback, connection);
+			});
+		} else {
+			query = conn.query(q, values, function (err, results) {
+				callback(err, results);
+				conn.release();
+			});
+		 	console.log(query.sql);
+			return self;
+		}
+	};
+
+	self.select = function (db, q, values, callback) {
+		db = db || 'SLAVE';
+		values = values || [];
+		callback = callback || function () { return false; };
+
+		self.query(db, q, values, function (err, results) {
+			if (err) {
+				throw err;
+			}
+			var ret = [];
+			for (var i=0,r; r = results[i]; i++) {
+				var t = table_obj(self.table);
+				for (var k in r) {
+					t[k] = r[k];
+				}
+				ret.push(t);
+			}
+			callback(err, ret);
+		});
+	}
+
+	self.insert = function (values, callback) {
+		callback = callback || function () { return false; };
+		var q = "INSERT INTO ?? SET ?";
+		var replace = [ self.table, values ];
+		self.query('MASTER', q, replace, function (err, result) {
+			if (typeof self.id !== 'undefined') {
+				self.id = result.insertId;
+			}
+			callback(err, result);
+		});
+		return self;
+	};
+
+	self.update = function (values, callback) {
+		callback = callback || function () { return false; };
+		var q = "UPDATE ?? SET ?";
+		var replace = [ self.table, values ];
+		self.query('MASTER', q, replace, callback);
+		return self;
+	};
+
+	self.save = function (values, callback) {
+		callback = callback || function () { return false; };
+		if (values[self.primaryKey] == null) {
+			self.insert(values, callback);
+		} else {
+			self.update(values, callback);
+		}
+		return self;
+	};
+
+	self.getByPk = function (value, callback) {
+		callback = callback || function () { return false; };
+		var q = "SELECT * FROM ?? WHERE ?? = ? LIMIT 1";
+		var values = [ self.table, self.primaryKey, value ];
+		self.select('SLAVE', q, values, function (err, rows) {
+			if (rows.length > 0) {
+				rows = rows[0];
+				for (var k in rows) {
+					self[k] = rows[k];
+				}
+				callback(err, rows);
+			} else {
+				callback(err, false);
+			}
+		});
+		return self;
+	};
+
+	self.getById = function (value, callback) {
+		callback = callback || function () { return false; };
+		var q = "SELECT * FROM ?? WHERE id = ? LIMIT 1";
+		var values = [ self.table, value ];
+		self.select('SLAVE', q, values, function (err, rows) {
+			if (rows.length > 0) {
+				rows = rows[0];
+				for (var k in rows) {
+					self[k] = rows[k];
+				}
+				callback(err, rows);
+			} else {
+				callback(err, false);
+			}
+		});
+		return self;
+	};
+
+	self.get = function (search_obj, callback) {
+		var	q = "SELECT ?? FROM ??",
+			limit = false,
+			offset = false,
+			orderby = false,
+			orderby_direction = false,
+			replace
+		;
+		if (search_obj.columns) {
+			replace = [ search_obj.columns, self.table ];
+			delete search_obj.columns;
+		} else {
+			replace = [ self.columns, self.table ];
+		}
+		if (search_obj.limit) {
+			limit = search_obj.limit;
+			delete search_obj.limit;
+		}
+		if (search_obj.offset) {
+			offset = search_obj.offset;
+			delete search_obj.offset;
+		}
+		if (search_obj.orderby) {
+			orderby = search_obj.orderby;
+			delete search_obj.orderby;
+		}
+		if (search_obj.orderby_direction) {
+			orderby_direction = search_obj.orderby_direction;
+			delete search_obj.orderby_direction;
+		}
+		if (size(search_obj) > 0) {
+			q += ' WHERE';
+			var first = true;
+			for (var k in search_obj) {
+				if (first) {
+					q += ' ??';
+					first = false;
+				} else {
+					q += ' AND ??';
+				}
+				replace.push(k);
+				if (typeof search_obj[k] == 'object') {
+					q += ' ' + search_obj[k].operator + ' ?';
+					replace.push(search_obj[k].value);
+				} else {
+					q += ' = ?';
+					replace.push(search_obj[k]);
+				}
+			}
+		}
+		if (orderby) {
+			q += ' ORDER BY ??';
+			replace.push(orderby);
+		}
+		if (orderby_direction) {
+			if (orderby_direction == 'DESC') {
+				q += ' DESC';
+			}
+		}
+		if (limit) {
+			q += ' LIMIT ?';
+			replace.push(limit);
+		}
+		if (offset) {
+			q += ' OFFSET ?';
+			replace.push(offset);
+		}
+		self.select('SLAVE', q, replace, callback);
+		return self;
+	};
+
+	self.join = function (joins, search_obj, callback) {
+		var	q = "SELECT ?? FROM ??",
+			limit = false,
+			offset = false,
+			orderby = false,
+			orderby_direction = false,
+			replace
+		;
+		if (search_obj.columns) {
+			replace = [ search_obj.columns, self.table ];
+			delete search_obj.columns;
+		} else {
+			replace = [ self.columns, self.table ];
+		}
+		if (search_obj.limit) {
+			limit = search_obj.limit;
+			delete search_obj.limit;
+		}
+		if (search_obj.offset) {
+			offset = search_obj.offset;
+			delete search_obj.offset;
+		}
+		if (search_obj.orderby) {
+			orderby = search_obj.orderby;
+			delete search_obj.orderby;
+		}
+		if (search_obj.orderby_direction) {
+			orderby_direction = search_obj.orderby_direction;
+			delete search_obj.orderby_direction;
+		}
+		for (var i=0,join; join = joins[i]; i++) {
+			if (join.type) {
+				q += ' ' + join.type;
+				delete join.type;
+			}
+			q += ' JOIN ?? ON ??';
+			replace.push(join.table);
+			replace.push(join.left);
+			q += (join.operator) ? ' ' + join.operator + ' ??' : ' = ??';
+			replace.push(join.right);
+		}
+		if (size(search_obj) > 0) {
+			q += ' WHERE ?';
+			replace.push(search_obj);
+		}
+		if (orderby) {
+			q += ' ORDERBY ??';
+			replace.push(orderby);
+		}
+		if (orderby_direction) {
+			q += ' ??';
+			replace.push(orderby_direction);
+		}
+		if (limit) {
+			q += ' LIMIT ?';
+			replace.push(limit);
+		}
+		if (offset) {
+			q += ' OFFSET ?';
+			replace.push(offset);
+		}
+		self.query('SLAVE', q, replace, callback);
+		return self;
+	};
+
+	self.deleteRecord = function (primaryKey, callback) {
+		callback = callback || function () { return false; };
+		var q = "DELETE FROM ?? WHERE ?? = ?";
+		var replace = [ self.table, self.primaryKey, primaryKey ];
+		self.query('MASTER', q, replace, callback);
+	};
+
+	self.init = function (table_name, obj, callback) {
+		callback = callback || function () { return false; };
+		if (tables[table_name]) {
+			var table_c = tables[table_name];
+			obj = obj || {};
+
+			table_c =  clone(table_c);
+			for (var k in obj) {
+				table_c[k] = obj[k];
+			}
+			callback(table_c);
+			return table_c;
+		} else {
+			var q = "DESCRIBE ??";
+			self.query('SLAVE', q, [ table_name ], function (err, res) {
+				if (err) { throw err; }
+				for (var i=0,field; field = res[i]; i++) {
+					self[field.Field] = null;
+					self.columns.push(field.Field);
+					if (field.key == 'PRI') {
+						self.primaryKey = field.Field
+					}
+				}
+				self.table = table;
+				tables[table_name] = clone(self);
+			});
+			while (!tables[table]) {
+				deasync.runLoopOnce();
+			}
+			return self.init(table, obj, callback);
+		}
+	};
+
+	return self.init(table, obj, callback);
+};
+
+function table (obj) {
+	var self = {};
+	for (var k in obj) {
+		self[k] = obj[k];
+	}
+	for (var i=0,c; c = self.columns[i]; i++) {
+		self[c] = null;
+	}
+	self.save = function (callback) {
+		callback = callback || function () { return false; };
+		gateway(self.table).save(self.getColumnValues(), callback);
+	};
+	self.deleteRecord = function (callback) {
+		callback = callback || function () { return false; };
+		gateway(self.table).deleteRecord(self[self.primaryKey], callback);
+	};
+	self.getColumnValues = function () {
+		var ret = {};
+		for (var i=0,c; c = self.columns[i]; i++) {
+			ret[c] = self[c];
+		}
+		return ret;
+	};
+	return self;
+}
+
+function table_obj (table_name) {
+	var t = gateway(table_name);
+	var obj = {
+		table: t.table,
+		columns: t.columns.slice(0),
+		primaryKey: t.primaryKey
+	};
+	return table(obj);
+}
+
+//helper functions
+var size = function(obj) {
+	var size = 0, key;
+	for (key in obj) {
+		if (obj.hasOwnProperty(key)) size++;
+	}
+	return size;
+};
+
+var initialize = false;
+pool.getConnection('SLAVE', function (err, conn) {
+	if (err) { throw err; }
+	var fieldname = 'Tables_in_' + conn.config.database;
+	conn.query('SHOW TABLES', [])
+		.on('error', function (err) {
+			throw err;
+		})
+		.on('result', function (row) {
+			conn.pause();
+			var table_name = row[fieldname];
+			gateway(table_name, {}, function (table) {
+				conn.resume();
+			});
+		})
+		.on('end', function () {
+			initialize = true;
+		})
+	;
+});
+
+while (!initialize) {
+	deasync.runLoopOnce();
+}
+
+//exports.gateway = gateway;
+exports.table = table_obj;
+exports.gateways = tables;
